8c8dd21ed8631dc307327e7ea485a248aeba8db6
Silvan Achtnich <35539054+8nich@users.noreply.github.com>
2021-03-18 16:58:42 +0100

    Delete Execute_covid19.bat
    not needed under version control

--- Execute_covid19.bat
+++
@@ -1,5 +0,0 @@
-TIMEOUT /T 10
-cd C:\Users\s8nic\Documents\Privat\Projekte\Python\RestAPI
-C:\Programme\Python39\python.exe c:/Users/s8nic/Documents/Privat/Projekte/Python/RestAPI/covid19.py
-rem rundll32.exe powrprof.dll,SetSuspendState 0,1,0 
-
